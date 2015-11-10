e0930e4a2fbdc779313614df18e0e7b31dca5059
Andreas Thinggaard Mouritsen <mouritsen93@outlook.com>
2015-11-10 11:22:14 +0100

    Project

---
+++ CentralAlarmSE/DataAccessLayer/Facade.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Database;
+using DataAccessLayer.Repositories;
+using DataAccessLayer.Repositories.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Facade
+    {
+        private IRepository<Alarm> alarmRepository; 
+
+        public IRepository<Alarm> GetAlarmRepository()
+        {
+            return alarmRepository ?? (alarmRepository = new AlarmRepository());
+        }
+    }
+}
